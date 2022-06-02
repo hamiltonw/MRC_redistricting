11ab25b9072c8536defb8bbfddbcb390d100b874
Wesley Hamilton <hamilton@math.utah.edu>
2022-06-02 10:32:29 -0400

    Add an example script that illustrates the hypothesis test

--- hypothesis_test.py
+++ hypothesis_test.py
@@ -1,18 +1,7 @@
 # for gerrychain
-
-import matplotlib.pyplot as plt
-from gerrychain import (GeographicPartition, Partition, Graph, MarkovChain,
-                        proposals, updaters, constraints, accept, Election)
-from gerrychain.proposals import recom
-from functools import partial
-import pandas as pd
-import geopandas as gpd
-
+import numpy as np
 from tqdm import tqdm
 
-# for fancy map plotting
-import folium
-import mapclassify
 
 """
 wrappers for some of the Gerrychain functions
@@ -79,18 +68,15 @@ def test_hypothesis(markov_chain,p,epsilon,k,w,election_name):
 
     all_results = [None for i in range(k+1)] #set some space for each plan
 
-    for idx, partition in enumerate(markov_chain): #run the chain 
+    for idx, partition in tqdm(enumerate(markov_chain)): #run the chain 
         all_results[idx] = partition
 
     #(3) Evaluate the partisan properties of each districting encountered (e.g., by simulating elections using past voting data).
 
-    w_vals = [w(p[election_name]) for p in all_results]
+    w_vals = np.array([w(p[election_name]) for p in all_results])
 
     #(4) Call the original districting “carefully crafted” or “gerrymandered” if the overwhelming majority of districtings produced by making small random changes are less partisan than the original districting.
 
     counts = sum(w_vals[1:]> w_vals[0])
 
-    if counts <= epsilon*(k+1):
-        return True 
-    else:
-        return False
+    return counts <= epsilon*(k+1), (counts,all_results)
