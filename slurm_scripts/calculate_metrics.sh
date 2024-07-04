399be57884eddc34986fb45643f8a6150b2a9952
Harlin Lee <36711897+HarlinLee@users.noreply.github.com>
2024-07-04 09:48:05 -0700

    Update calculate_metrics.sh

--- slurm_scripts/calculate_metrics.sh
+++ slurm_scripts/calculate_metrics.sh
@@ -11,28 +11,3 @@ cd MRC_redistricting
 ##### calculate and save biased chain metrics
 python -u ./calculate_metrics.py --state NC 
 python -u make_hists.py --state NC --s 50
-
-##### histograms
-#python -u ./make_hists.py --party Dem --sb 1 --state TX --n 1
-#python -u ./make_hists.py --party Rep --sb 1 --state TX --n 1
-
-#python -u ./make_hists.py --party Dem --sb 1 --state NC --n 1
-#python -u ./make_hists.py --party Rep --sb 1 --state NC --n 1
-
-#python -u ./make_hists.py --party Dem --sb 1 --state WI --n 1
-#python -u ./make_hists.py --party Rep --sb 1 --state WI --n 1
-
-#python -u ./make_hists.py --party Dem --sb 1 --state MD --n 1
-#python -u ./make_hists.py --party Rep --sb 1 --state MD --n 1
-
-##### heat maps
-#python -u ./make_heats.py --sb 1 --state TX --n 1
-#python -u ./make_heats.py --sb 1 --state NC --n 1
-#python -u ./make_heats.py --sb 1 --state WI --n 1
-#python -u ./make_heats.py --sb 1 --state MD --n 1
-
-##### scatter plots
-#python -u ./make_scats.py --sb 1 --state TX --n 100
-#python -u ./make_scats.py --sb 1 --state NC --n 100
-#python -u ./make_scats.py --sb 1 --state WI --n 100
-#python -u ./make_scats.py --sb 1 --state MD --n 100
